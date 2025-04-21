52399ad026d07b49a8eed64ff52232d65c5c528c
aaronBIOO <aaronnabioo@gmail.com>
2025-04-21 17:19:44 +0000

    API correctly setup. Resolving error handling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 function Search({ searchTerm, setSearchTerm }) {
   return (
     <div className='search'>
-      <img src='search.svg' alt='search' />
-      
-      <input
-        type='text'
-        placeholder='Search through thousands of movies'
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <div>
+        <img src='search.svg' alt='search' />
+        
+        <input
+          type='text'
+          placeholder='Search through thousands of movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Search;
