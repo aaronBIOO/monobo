e63860f7c4dddc14ecff1b17dabb1925f2b2f560
aaronBIOO <aaronnabioo@gmail.com>
2025-04-21 22:35:27 +0000

    Implemented movie cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 // API setup initialized
 const API_BASE_URL = 'https://api.themoviedb.org/3';
@@ -84,7 +85,7 @@ function App() {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p key={movie.id} className="text-white">{movie.title}</p>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
