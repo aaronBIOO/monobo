a474639548a812a8d1bf3c51c6f54393d973402b
aaronBIOO <aaronnabioo@gmail.com>
2025-04-21 07:53:06 +0000

    my intial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper"> 
+        <header>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
