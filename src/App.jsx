1a2521141542bb34aaa90d6025bb4c13f8f6f0e7
aaronBIOO <aaronnabioo@gmail.com>
2025-04-21 18:33:39 +0000

    update to error handling

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,8 @@ function App() {
 
       setMovieList(data.results || []);
     } catch (error) {
-      console.error(`Error: fetching movies interrupted: ${error}`);
-      setErrorMessage('Error: fetching movies interrupted. Please try again later.');
+      console.error(`Error: fetching movies was interrupted: ${error}`);
+      setErrorMessage('Error: fetching movies was interrupted. Please try again later.');
     } finally {
       setIsLoading(false);
     }
