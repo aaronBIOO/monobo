ef9e656b81092d73cdf87c1b4209f5fd7100dc7d
aaronBIOO <aaronnabioo@gmail.com>
2025-04-22 22:52:12 +0000

    update: scrapped MovieCard top tittle

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 function MovieCard({ movie: { title, vote_average, poster_path, release_date, original_language } }) {
   return (
-    <div className='movie-card'>
-        <p className='text-white'>{title}</p>
+    <div className='movie-card'>~
         <img src={poster_path ? 
         `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} 
         alt={title}
